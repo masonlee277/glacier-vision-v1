36dd19c6e96ee80332c7739c42b7db136947c72a
masonlee277 <46269622+masonlee277@users.noreply.github.com>
2024-09-08 15:30:44 -0400

    refactor prediction

--- api/config.py
+++ api/config.py
@@ -1,5 +1,5 @@
 import os
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     PROJECT_ROOT: str = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
